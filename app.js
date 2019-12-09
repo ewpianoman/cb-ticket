3c0f3403635f230f62d19071215b9170bf84b2c9
Eric Wilson <ewpiano@gmail.com>
2019-12-08 18:39:08 -0600

    Initial setup - created app.js, initialized npm and git.

---
+++ app.js
@@ -0,0 +1,18 @@
+// Require packages
+const express = require('express');
+
+// Make new instance of express.
+const app = express();
+const bodyparser = require('body-parser');
+
+// Create port for server.
+const port = process.env.PORT || 3200;
+
+// Middleware
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server listening at port ${port}.`);
+});
