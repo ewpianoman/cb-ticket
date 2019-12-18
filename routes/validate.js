aa5436f2341a577995afebc508e9677d2085ac76
Eric Wilson <ewpiano@gmail.com>
2019-12-18 14:37:52 -0600

    Added validation middleware.

---
+++ routes/validate.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const User = require('../models/user');
+
+const validate = (req, res, next) => {
+  const email = req.body.user.email;
+  let existingUser = User.findOne({ email: email }, (err, user) => {
+    if (err) {
+      return err;
+    }
+    if (user) {
+      return res.status(422).json({
+        errors: {
+          user: 'already exists'
+        }
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+module.exports = validate;
